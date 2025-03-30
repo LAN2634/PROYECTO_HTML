4485e69e0f7b92c0b4dd9feb218ac41deacaef8e
Danna <dan.prog16@gmail.comgit config --global user.name Danna>
2025-03-30 02:12:46 -0600

    solo anadi el visor de pdf para libros

---
+++ JS/script_pdf.js
@@ -0,0 +1,12 @@
+function toggleFullScreen() {
+    let iframe = document.querySelector("iframe");
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen();
+    }
+}
